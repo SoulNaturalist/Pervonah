dc6fb23dbdaf384f841997e4cc963f2f9c2ff6ee
Saywex89 <58743402+Saywex89@users.noreply.github.com>
2020-11-06 21:15:32 +0700

    update limits

--- Pervonah_bot.py
+++ Pervonah_bot.py
@@ -1,63 +1,70 @@
 import vk
 import random
 import time
+from datetime import datetime, timedelta
 from random import randint
 from time import gmtime, strftime
 
 
 
-token = input('Введите вк токен:')
+token = '76beddabb91bc38a1c491763e0ec168a3034f8955e9c2e70bf51e129c6fa67da80fc017b669a28a2a06b7'
+
+mess_unput = [
+    'Не понял',
+    'поясните',
+    'а в чом прикол?',
+    'А в чём шутка?',
+    'пояснительная бригада помоги!!!!',
+    'хм'
 
 
-mess_unput = input('Введите сообщения:')
+]
 
-group_unput = input('Введите id групп:')
-
-photos = input('Введите фото:')
-list_ids = []
-session = vk.Session(access_token=token)
-api = vk.API(session ,v='5.92', lang='ru')
-while True:
-    time.sleep(2.2)
-
-    post = api.newsfeed.get(filters='post',counts=1,source_ids=group_unput.split(','))
-
-    postID = post['items'][0]['post_id']
-
-    sourceID = post['items'][0]['source_id']
-
-    like = post['items'][0]['likes']['user_likes']
-
-    mess = mess_unput.split(',')
-
-    photo = photos.split(',')
-
-
-    mess_generate = mess[randint(0,len(mess)-1,)]
-
-
-    photo_generate = random.choice(photo)
+photos = [
     
-    with open("ids.txt") as file:
+]
 
-        ids = file.read().split(",")
-        
-    for _ in range(len(ids)):
+
+list_ids = []
+
+session = vk.Session(access_token=token)
+
+api = vk.API(session, v='5.92', lang='ru')
+
+groups  = api.groups.get(filter='groups, publics', count=150)
+
+groups_ids = groups['items'] #получение подписок пользователя 
+
+while True:
+    time.sleep(6.4)
+
+    post = api.wall.get(filters='owner',counts=1,owner_id=f'-{random.choice(groups_ids)}')
+
+    comments = post['items'][0]['comments']['count']
+
+    like = post['items'][0]['likes']['count']
+   
+    if comments < 5 and like < 90:
+
+        postID = post['items'][0]['id']
+
+        sourceID = post['items'][0]['owner_id']
         try:
             if postID not in list_ids:
+                message_text = random.choice(mess_unput)
                 if photos == '-' or photos == '':
                     list_ids.append(postID)
-                    comment = api.wall.createComment(owner_id=ids[_-1],post_id=postID,message=mess_generate)
-                    comment_id = comment['comment_id']
-                    api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
-                    print('Комментарий оставлен ' + 'https://vk.com/wall' + str(sourceID) + '_' + str(postID) + ' |' + str(strftime('[%H:%M:%S]')) + ' |' + 'Сообщение оставил : ' + mess_generate)
-                
+                    api.wall.createComment(owner_id=sourceID,post_id=postID,message=message_text)
+                    #comment_id = comment['comment_id']
+                    #api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
+                    print('Комментарий оставлен ' + 'https://vk.com/wall' + str(sourceID) + '_' + str(postID) + ' |' + str(strftime('[%H:%M:%S]')) + ' |' + 'Сообщение оставил : ' +  message_text)
+                    
                 else:
+                    random_photo = random.choice(photos)
                     list_ids.append(postID)
-                    comment = api.wall.createComment(owner_id=ids[_-1],post_id=postID,message=mess_generate,attachments=photo_generate)
-                    comment_id = comment['comment_id'] 
-                    api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
-                    print('Комментарий оставлен ' + 'https://vk.com/wall' + str(sourceID) + '_' + str(postID) + ' |' + str(strftime('[%H:%M:%S]')) + ' |' + 'Сообщение оставил : ' + mess_generate)
-                   
-        except vk.exceptions.VkAPIError:
-            pass
+                    api.wall.createComment(owner_id=sourceID,post_id=postID,message=message_text, attachments=random_photo)
+                    #comment_id = comment['comment_id'] 
+                    #api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
+                    print('Комментарий оставлен ' + 'https://vk.com/wall' + str(sourceID) + '_' + str(postID) + ' |' + str(strftime('[%H:%M:%S]')) + ' |' + 'Сообщение оставил : ' +  message_text)
+        except vk.exceptions.VkAPIError as error:
+            print(error)
