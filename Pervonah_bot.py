b1ec39457f92094ab78c0d23b69e72a54e90d7f1
Saywex89 <omg12121@mail.ru>
2020-12-21 16:08:31 +0700

    switched from vk to vk-api and added a check for captcha

--- Pervonah_bot.py
+++ Pervonah_bot.py
@@ -1,11 +1,13 @@
-import vk
+import vk_api 
 import random
 import time
 from time import gmtime, strftime
 
+LOGIN = 'number'
 
+PASSW = 'user password'
 
-token = 'Вставь свой токен'
+DELAY = 4
 
 mess_unput = [
 
@@ -18,16 +20,18 @@ photos = [
 
 list_ids = []
 
-session = vk.Session(access_token=token)
+api = vk_api.VkApi(LOGIN, PASSW)
 
-api = vk.API(session, v='5.92', lang='ru')
+api.auth()
 
-groups  = api.groups.get(filter='groups, publics', count=150)
+api = api.get_api()
 
-groups_ids = groups['items'] #получение подписок пользователя 
+groups  = api.groups.get(filter='groups, publics', count=500)
+
+groups_ids = groups['items'] 
 
 while True:
-    time.sleep(6.4)
+    time.sleep(DELAY)
 
     post = api.wall.get(filters='owner',counts=1,owner_id=f'-{random.choice(groups_ids)}')
 
@@ -41,21 +45,28 @@ while True:
 
         sourceID = post['items'][0]['owner_id']
         try:
-            if postID not in list_ids:
-                message_text = random.choice(mess_unput)
-                if not photos:
-                    list_ids.append(postID)
-                    comment = api.wall.createComment(owner_id=sourceID,post_id=postID,message=message_text)
-                    comment_id = comment['comment_id']
-                    api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
-                    print('Комментарий оставлен ' + 'https://vk.com/wall' + str(sourceID) + '_' + str(postID) + ' |' + str(strftime('[%H:%M:%S]')) + ' |' + 'Сообщение оставил : ' +  message_text)
-                    
-                else:
-                    random_photo = random.choice(photos)
-                    list_ids.append(postID)
-                    comment = api.wall.createComment(owner_id=sourceID,post_id=postID,message=message_text, attachments=random_photo)
-                    comment_id = comment['comment_id'] 
-                    api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
-                    print('Комментарий оставлен ' + 'https://vk.com/wall' + str(sourceID) + '_' + str(postID) + ' |' + str(strftime('[%H:%M:%S]')) + ' |' + 'Сообщение оставил : ' +  message_text)
-        except vk.exceptions.VkAPIError as error:
-            print(error)
+            try:
+                if postID not in list_ids:
+                    message_text = random.choice(mess_unput)
+                    if not photos:
+                        list_ids.append(postID)
+                        comment = api.wall.createComment(owner_id=sourceID,post_id=postID,message=message_text)
+                        comment_id = comment['comment_id']
+                        api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
+                        print('Комментарий оставлен ' + 'https://vk.com/wall' + str(sourceID) + '_' + str(postID) + ' |' + str(strftime('[%H:%M:%S]')) + ' |' + 'Сообщение оставил : ' +  message_text)
+                        
+                    else:
+                        random_photo = random.choice(photos)
+                        list_ids.append(postID)
+                        comment = api.wall.createComment(owner_id=sourceID,post_id=postID,message=message_text, attachments=random_photo)
+                        comment_id = comment['comment_id'] 
+                        api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
+                        print('Комментарий оставлен ' + 'https://vk.com/wall' + str(sourceID) + '_' + str(postID) + ' |' + str(strftime('[%H:%M:%S]')) + ' |' + 'Сообщение оставил : ' +  message_text)
+            except api.exceptions.Captcha as captcha:
+                captcha.sid  
+                print(f'Появилась капча - {captcha.get_url()}')
+                captcha_key = input('Введите капчу:')
+                captcha.try_again(captcha_key)
+        except Exception as error:
+            print(f'Ошибка {error}')
+
