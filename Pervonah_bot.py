12b75d72368ee19cfecc82b6c660a770d28b42cc
Saywex89 <omg12121@mail.ru>
2021-01-07 12:00:31 +0700

    add func send only photo/media content

--- Pervonah_bot.py
+++ Pervonah_bot.py
@@ -7,13 +7,13 @@ from time import gmtime, strftime
 from colorama import Fore, Back, Style
 
 
-LOGIN = 'your login'
+LOGIN = 'login'
 
 PASSW = 'password'
 
 DELAY = 6.6
 
-list_message = ['1 message', '2 message']
+list_message = []
 
 photos = []
 
@@ -27,7 +27,7 @@ BANNER = '''
 
 active_sesion = requests.Session()
 
-active_sesion.proxies.update({'http': 'http://proxy:port'})
+active_sesion.proxies.update({'http': 'http://203.30.189.46:80'})
 
 api = vk_api.VkApi(LOGIN, PASSW, session=active_sesion)
 
@@ -53,37 +53,53 @@ print(Fore.BLUE + BANNER)
 print(f'Активный аккаунт {acount_name} {acount_lastname} {acount_id}ID')
 
 while True:
-    time.sleep(DELAY)
+    try:
+        time.sleep(DELAY)
 
-    post = api.wall.get(filters='owner',counts=1,owner_id=f'-{random.choice(groups_ids)}')
+        post = api.wall.get(filters='owner',counts=1,owner_id=f'-{random.choice(groups_ids)}')
 
-    comments = post['items'][0]['comments']['count']
+        comments = post['items'][0]['comments']['count']
 
-    like = post['items'][0]['likes']['count']
-   
-    if comments < 5 and like < 90:
+        like = post['items'][0]['likes']['count']
+    
+        if comments < 5 and like < 90:
+            postID = post['items'][0]['id']
 
-        postID = post['items'][0]['id']
-
-        sourceID = post['items'][0]['owner_id']
-        try:
+            sourceID = post['items'][0]['owner_id']
             if postID not in list_ids:
-                message_text = random.choice(list_message)
                 if not photos:
                     list_ids.append(postID)
+                    message_text = random.choice(list_message)
                     comment = api.wall.createComment(owner_id=sourceID,post_id=postID,message=message_text)
                     comment_id = comment['comment_id']
                     api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
-                    
-                    print(f'''\n-----------------------------------------------------------------
-                    \nКомментарий оставлен ✅
-                    \nСсылка на комментарий - https://vk.com/wall{sourceID}_{postID}
-                    \nКогда был оставлен комментарий - {strftime('%H:%M:%S')}
-                    \nСообщение: {message_text}''')
+                    print(f'''
+                    -----------------------------------------------------------------
+                    Комментарий оставлен ✅
+                    Ссылка на комментарий - https://vk.com/wall{sourceID}_{postID}
+                    Когда был оставлен комментарий - {strftime('%H:%M:%S')}
+                    Сообщение: {message_text}
+                    -----------------------------------------------------------------
+                    ''')
+                elif not list_message:
+                    list_ids.append(postID)
+                    random_photo = random.choice(photos)
+                    comment = api.wall.createComment(owner_id=sourceID,post_id=postID,attachments=random_photo)
+                    comment_id = comment['comment_id']
+                    api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
+                    print(f'''
+                    -----------------------------------------------------------------
+                    Комментарий оставлен ✅
+                    Ссылка на комментарий - https://vk.com/wall{sourceID}_{postID}
+                    Когда был оставлен комментарий - {strftime('%H:%M:%S')}
+                    Сообщение: {message_text}
+                    -----------------------------------------------------------------
+                    ''')
                         
                 else:
-                    random_photo = random.choice(photos)
                     list_ids.append(postID)
+                    random_photo = random.choice(photos)
+                    message_text = random.choice(list_message)
                     comment = api.wall.createComment(owner_id=sourceID,post_id=postID,message=message_text, attachments=random_photo)
                     comment_id = comment['comment_id'] 
                     api.likes.add(type='comment', owner_id=sourceID, item_id=comment_id)
@@ -96,16 +112,17 @@ while True:
                     -----------------------------------------------------------------
                     ''')
                    
-        except vk_api.exceptions.Captcha as captcha:
-            captcha.sid  
-            print(f'Появилась капча - {captcha.get_url()}')
-            captcha_key = input('Введите капчу:')
-            captcha.try_again(captcha_key)
-        except vk_api.exceptions.AccountBlocked:
-            print('Ваш аккаунт заблокировали')
-            break
-        except vk_api.exceptions.VkApiError:
-            pass
+    except vk_api.exceptions.Captcha as captcha:
+        captcha.sid  
+        print(f'Появилась капча - {captcha.get_url()}')
+        captcha_key = input('Введите капчу:')
+        captcha.try_again(captcha_key)
+    except vk_api.exceptions.AccountBlocked:
+        print('Ваш аккаунт заблокировали')
+        break
+    except vk_api.exceptions.VkApiError:
+        pass
+
 
 
 
